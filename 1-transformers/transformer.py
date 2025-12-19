f47f93ae05dfc2e367b0132073bfbba856234ac1
johnrfriess <johnrfriess@gmail.com>
2025-12-18 23:12:15 -0500

    finished single head transformer

--- 1-transformers/transformer.py
+++ 1-transformers/transformer.py
@@ -1,9 +1,11 @@
 # decoder only transformer
 import torch
+import re
 from jaxtyping import Int, Float
 
 class Attention(torch.nn.Module):
     def __init__(self, d_model=64, d_k=16, d_v=16, n_heads=1):
+        super().__init__()
         self.d_k = d_k
         self.w_q = torch.nn.Parameter(torch.randn(d_model, d_k))
         self.w_k = torch.nn.Parameter(torch.randn(d_model, d_k))
@@ -30,6 +32,7 @@ class Attention(torch.nn.Module):
 
 class FFN(torch.nn.Module):
     def __init__(self, d_model=64):
+        super().__init__()
         self.linear1 = torch.nn.Linear(d_model, d_model)
         self.relu = torch.nn.ReLU()
         self.linear2 = torch.nn.Linear(d_model, d_model)
@@ -42,6 +45,7 @@ class FFN(torch.nn.Module):
 
 class Transformer(torch.nn.Module):
     def __init__(self, n_layers=1, d_model=64, d_k=16, d_v=16, n_heads=1, vocab_size=5000):
+        super().__init__()
         self.d_model = d_model
         self.n_heads = n_heads
         self.embedding = torch.nn.Parameter(torch.randn(vocab_size, d_model))
@@ -49,6 +53,7 @@ class Transformer(torch.nn.Module):
         self.ln1 = torch.nn.LayerNorm(d_model)
         self.ffn = FFN(d_model)
         self.ln2 = torch.nn.LayerNorm(d_model)
+        self.linear = torch.nn.Linear(d_model, vocab_size)
 
     def forward(self, x: Int[torch.Tensor, "token"]) -> Float[torch.Tensor, "token d_model"]:
         x = self.embedding[x]
@@ -56,6 +61,7 @@ class Transformer(torch.nn.Module):
 
         x = self.ln1(x + self.attention(x, x, x))
         x = self.ln2(x + self.ffn(x))
+        x = self.linear(x)
         return x
 
     def pos_embedding(self, x: Float[torch.Tensor, "token d_model"]) -> Float[torch.Tensor, "token d_model"]:
@@ -69,8 +75,73 @@ class Transformer(torch.nn.Module):
         return pos_embedding
 
 
-transformer = Transformer()
-# print(transformer.pos_embedding(torch.as_tensor([[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8], [5, 6, 7, 8]])))
-q = torch.as_tensor([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]])
-# v = torch.as_tensor([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]])
-print(transformer.attention(q, q, q))
+PAD = "<pad>"
+UNK = "<unk>"
+
+def tokenize(text: str):
+    tokens = re.split(r"\b", text)
+    tokens = [t for t in tokens if t != "" and not t.isspace()]
+    return tokens
+
+def build_vocab(tokens, min_freq=1):
+    cnt = dict()
+    for token in tokens:
+        cnt[token] = cnt.get(token, 0) + 1
+    
+    vocab = [PAD, UNK] + [t for t, c in cnt.items() if c >= min_freq and t not in (PAD, UNK)]
+    stoi = {t:i for i,t in enumerate(vocab)}
+    itos = {i:t for t,i in stoi.items()}
+    return stoi, itos
+
+def numericalize(tokens, stoi):
+    unk_id = stoi[UNK]
+    return [stoi.get(t, unk_id) for t in tokens]
+
+with open('shakespeare.txt', 'r') as f:
+    content = f.read()
+
+tokens = tokenize(content)
+stoi, itos = build_vocab(tokens)
+vocab_size = len(stoi)
+
+model = Transformer(d_model=8, d_k=4, vocab_size=vocab_size)
+optimizer = torch.optim.Adam(model.parameters())
+seq_len = 32
+print(len(tokens))
+for epoch in range(1):
+    print(f"training epoch {epoch}")
+    for i in range(0, 100000, seq_len):
+        print(f"training seq start {i}")
+        start, end = i, i+seq_len
+        if end > len(tokens):
+            continue
+        seq = torch.as_tensor(numericalize(tokens[start:end], stoi))
+        x = seq[:-1]
+        targets = seq[1:]
+        optimizer.zero_grad()
+        logits = model(x)
+        loss = torch.functional.F.cross_entropy(logits, targets)
+        loss.backward()
+        optimizer.step()
+
+with torch.no_grad():
+    for _ in range(5):
+        start = ["By", "unions", "married", "do", "offend", "thine"]
+        for _ in range(20):
+            x = torch.as_tensor(numericalize(start, stoi))
+            logits = model(x)
+            probs = torch.softmax(logits[-1], dim=-1)
+            next_word = itos[torch.multinomial(probs, 1).item()]
+            start.append(next_word)
+        print(start)
+
+    # for i in range(1000, 1100, seq_len):
+    #     start, end = i, i+seq_len
+    #     if end > len(tokens):
+    #         continue
+
+    #     x = torch.as_tensor(numericalize(tokens[start:end], stoi))
+    #     logits = model(x)
+    #     probs = torch.softmax(logits[-1], dim=-1)
+    #     next_word = itos[torch.multinomial(probs, 1).item()]
+    #     print(f"sequence: {" ".join(tokens[start:end])}, predicted: {next_word}")
